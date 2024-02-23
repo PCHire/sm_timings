086afb32b74e5fe6c43c6b9f3ff206aa784892b6
pchire <ben.claydon2000@gmail.com>
2024-02-23 11:08:28 +0000

    Fixed maven, changed functions to doubles

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,40 +11,40 @@ public class Main {
     static final int DATA_POINTS = 1000;
 
     // Distance functions. None are square rooted, but of course you could for each!
-    static float sparseTriDistance(float[] a, float[] b) {
-        float accumulator = 0f;
+    static double sparseTriDistance(double[] a, double[] b) {
+        double accumulator = 0d;
 
         for (int i = 0; i < a.length; i++) {
-            if (a[i] != 0 && b[i] != 0) {
+            if (a[i] != 0.0 && b[i] != 0.0) {
                 accumulator += (2 * a[i] * b[i]) / (a[i] + b[i]);
             }
         }
 
-        return 1 - accumulator;
+        return 1.0d - accumulator;
     }
 
-    static float triDistance(float[] a, float[] b) {
-        float accumulator = 0f;
+    static double triDistance(double[] a, double[] b) {
+        double accumulator = 0d;
 
         for (int i = 0; i < a.length; i++) {
             accumulator += (2 * a[i] * b[i]) / (a[i] + b[i]);
         }
 
-        return 1 - accumulator;
+        return 1.0d - accumulator;
     }
 
 
-    static float euclideanDistance(float[] a, float[] b) {
-        float accumulator = 0f;
+    static double euclideanDistance(double[] a, double[] b) {
+        double accumulator = 0d;
 
         for (int i = 0; i < a.length; i++) {
-            accumulator += (float) Math.pow(a[i] - b[i], 2);
+            accumulator += Math.pow(a[i] - b[i], 2);
         }
 
         return accumulator;
     }
 
-    static long measureSparseSM(ArrayList<float[]> data) {
+    static long measureSparseSM(ArrayList<double[]> data) {
         long start = System.currentTimeMillis();
         for (int i = 0; i < DATA_POINTS; i++) {
             for (int j = 0; j < i; j++) {
@@ -77,7 +77,7 @@ public class Main {
         return end - start;
     }
 
-    static long measureFullSM(ArrayList<float[]> data) {
+    static long measureFullSM(ArrayList<double[]> data) {
         long start = System.currentTimeMillis();
         for (int i = 0; i < DATA_POINTS; i++) {
             for (int j = 0; j < i; j++) {
@@ -89,7 +89,7 @@ public class Main {
         return end - start;
     }
 
-    static long measureFC6(ArrayList<float[]> data) {
+    static long measureFC6(ArrayList<double[]> data) {
         long start = System.currentTimeMillis();
         for (int i = 0; i < DATA_POINTS; i++) {
             for (int j = 0; j < i; j++) {
@@ -101,7 +101,7 @@ public class Main {
         return end - start;
     }
 
-    static ArrayList<float[]> loadMat(String path) throws IOException {
+    static ArrayList<double[]> loadMat(String path) throws IOException {
         Mat5File data = Mat5.readFromFile(path);
         Matrix features;
 
@@ -112,13 +112,13 @@ public class Main {
         }
 
 
-        ArrayList<float[]> listData = new ArrayList<>();
+        ArrayList<double[]> listData = new ArrayList<>();
         int dimensions = features.getNumCols();
 
         for (int row = 0; row < DATA_POINTS; row++) {
-            float[] point = new float[features.getNumCols()];
+            double[] point = new double[features.getNumCols()];
             for (int col = 0; col < dimensions; col++) {
-                point[col] = features.getFloat(row, col);
+                point[col] = features.getDouble(row, col);
             }
             listData.add(point);
         }
@@ -127,11 +127,12 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        ArrayList<float[]> smData = loadMat("/home/bc89/Documents/data/mf_softmax/0.mat");
-        ArrayList<float[]> prunedSmData = loadMat("/home/bc89/Documents/data/mf_softmax_pruned/0.mat");
-        ArrayList<float[]> fc6Data = loadMat("/home/bc89/Documents/data/mf_alexnet_fc6/0.mat");
+        ArrayList<double[]> smData = loadMat("/home/bc89/Documents/data/mf_softmax/0.mat");
+        ArrayList<double[]> prunedSmData = loadMat("/home/bc89/Documents/data/mf_softmax_pruned/0.mat");
+        ArrayList<double[]> fc6Data = loadMat("/home/bc89/Documents/data/mf_alexnet_fc6/0.mat");
 
-        List<SparseRep> sparsePrunedSMData = prunedSmData.stream().map(a -> new SparseRep( toDoubles(a) ) ).collect(Collectors.toList());
+        List<SparseRep> sparsePrunedSMData = prunedSmData.stream().map(a -> new SparseRep( a ) ).collect(Collectors.toList());
+
 
 
         long sm = measureFullSM(smData);
@@ -145,12 +146,14 @@ public class Main {
             sm = measureFullSM(smData);
             pruned = measureSparseSM(prunedSmData);
             fc6 = measureFC6(fc6Data);
+            al1 = measureSparseSM0Impl0(sparsePrunedSMData);
+            al2 = measureSparseSMImpl1(sparsePrunedSMData);
         }
 
         int repetitions = 20;
 
         long smTime = 0;
-        long prunedSmTimes= 0;
+        long prunedSmTimes = 0;
         long fc6Times = 0;
 
         for (int i = 0; i < repetitions; i++) {
@@ -165,16 +168,10 @@ public class Main {
             fc6Times += fc6;
         }
 
-        System.out.println("Softmax took " + smTime / repetitions  + " ms on average over " + repetitions + " repetitions");
-        System.out.println("Softmax Pruned took " + prunedSmTimes / repetitions  + " ms on average over " + repetitions + " repetitions");
-        System.out.println("Al Softmax Pruned0 took " + prunedSmTimes / repetitions  + " ms on average over " + repetitions + " repetitions");
-        System.out.println("Al Softmax Pruned1 took " + prunedSmTimes / repetitions  + " ms on average over " + repetitions + " repetitions");
-        System.out.println("Softmax took " + fc6Times / repetitions  + " ms on average over " + repetitions + " repetitions");
-    }
-
-    private static double[] toDoubles(float[] floats) {
-        double[] doubles = new double[floats.length];
-        for( int i = 0; i < floats.length; i++ ) { doubles[i] = floats[i]; }
-        return doubles;
+        System.out.println("Softmax took " + smTime / repetitions + " ms on average over " + repetitions + " repetitions");
+        System.out.println("Softmax Pruned took " + prunedSmTimes / repetitions + " ms on average over " + repetitions + " repetitions");
+        System.out.println("Al Softmax Pruned0 took " + al1 / repetitions + " ms on average over " + repetitions + " repetitions");
+        System.out.println("Al Softmax Pruned1 took " + al2 / repetitions + " ms on average over " + repetitions + " repetitions");
+        System.out.println("Softmax took " + fc6Times / repetitions + " ms on average over " + repetitions + " repetitions");
     }
 }
